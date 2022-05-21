22a4fd916db6c32fb5897ac039809aeb3d19c74f
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-22 01:29:25 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -9,6 +9,24 @@ const app = express();
 //middleware
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cw6pj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("mongodb connect");
+  } finally {
+    //   client.close();
+  }
+}
+
+run().catch(console.dir);
 
 //root api
 app.get("/", (req, res) => {
