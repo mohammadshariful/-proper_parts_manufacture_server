3353be2339ea7c712855ff2b697a1ad5dc94bb30
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-24 11:06:26 +0600

    verify admin middleware api created

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ async function run() {
     const purchaseCollection = client.db("proper_parts").collection("purchase");
     const reviewsCollection = client.db("proper_parts").collection("reviews");
     const usersCollection = client.db("proper_parts").collection("users");
+
+    //verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    };
     /* --------------Tools Collection Api Start----------------------- */
     //tools get api
     app.get("/tools", async (req, res) => {
@@ -108,7 +121,7 @@ async function run() {
       res.send(users);
     });
     //make admin user api
-    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const updateDoc = {
