08f464c95974af7749418d7d1193adac1e804934
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-25 23:10:54 +0600

    completed server side

--- index.js
+++ index.js
@@ -191,6 +191,12 @@ async function run() {
 
     /* --------------Reviews Collection Api End----------------------- */
     /* --------------User Collection Api Start----------------------- */
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
